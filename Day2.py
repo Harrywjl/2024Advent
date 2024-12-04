91f8dd04e4621592d8dabfc8a4dce9368dc5d116
Harrywjl <146112685+Harrywjl@users.noreply.github.com>
2024-12-04 13:17:58 -0500

    Update Day2.py

--- Day2.py
+++ Day2.py
@@ -7,6 +7,8 @@ def get_file_data(file_name):
 
 file_data = get_file_data("Day2Input.txt")
 
+#Part 1
+
 safe_count = 0
 unsafe_count = 0
 for i in file_data:
@@ -14,19 +16,14 @@ for i in file_data:
     prev_level = int(report[0])
     prev_difference = 0
     safe = True
-    print("Report")
-    print(" Level 0: " + str(prev_level))
     for x in range (1,len(report)):
         current_level = int(report[x])
-        print(" Level " + str(x) + ": " + str(current_level))
         current_difference = current_level - prev_level
-        print(" Dif: " + str(current_difference))
         if x > 1:
-            #if opposite signs or the same
-            if (prev_difference < 0 < current_difference) or (prev_difference > 0 > current_difference) or (prev_difference == current_difference):
-                #if
-                #levels differ by more than 3
+            if (prev_difference <= 0 <= current_difference) or (prev_difference >= 0 >= current_difference):
                     safe = False
+        if (abs(current_difference) == 0) or (abs(current_difference) > 3):
+            safe = False
         prev_level = current_level
         prev_difference = current_difference
     if safe:
