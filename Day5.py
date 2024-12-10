f2d3c60905aae24d16b2ee64e619e6c16a3ff3c0
Harrywjl <146112685+Harrywjl@users.noreply.github.com>
2024-12-10 13:21:27 -0500

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -1,3 +1,6 @@
+import math
+
+
 def get_file_data(file_name):
     f = open(file_name)
     data = []
@@ -11,12 +14,29 @@ file_data = get_file_data("Day5Input.txt")
 pages = []
 x_rule = []
 y_rule = []
+middle_pages = 0
 for line in range(file_data.index("") + 1, len(file_data)):
     row = file_data[line].split(",")
     pages.append(row)
 
 for i in range(0, file_data.index("")):
     rule = file_data[i]
-    x_rule.append(rule[0, rule.index(",")])
-    y_rule.append(rule[rule.index(",") + 1])
+    x_rule.append(rule[0:rule.index("|")])
+    y_rule.append(rule[rule.index("|") + 1])
 
+valid_pages = []
+for page in pages:
+    valid = True
+    i = 0
+    while i < len(page):
+        num = int(page[i])
+        rules = filter(lambda x:x == num,x_rule)
+        print(str(rules))
+
+    if valid:
+        valid_pages.append(page)
+
+for page in valid_pages:
+    middle_pages += page[math.floor(len(page) / 2)]
+
+print(middle_pages)
