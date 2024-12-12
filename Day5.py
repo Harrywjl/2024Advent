a1fd2e88f6256e6949803cce81120c33c34df0a5
Harrywjl <146112685+Harrywjl@users.noreply.github.com>
2024-12-12 13:20:25 -0500

    Update Day5.py

--- Day5.py
+++ Day5.py
@@ -8,20 +8,43 @@ def get_file_data(file_name):
         data.append(line.rstrip())
     return data
 
+
 def get_indexes(search_num, search_list):
     indexes_list = []
     i = 0
     while i < len(search_list):
         if search_list[i] == search_num:
             indexes_list.append(i)
-        i+=1
+        i += 1
     return indexes_list
 
-def breaks_x_rule(page, cur_index, rule_indexes):
 
-def breaks_y_rule(page, cur_index, rule_indexes):
+def comes_after(cur_page, cur_index, rule_indexes):
+    cur_num = cur_page[cur_index]
+    rules = []
+    for y in rule_indexes:
+        rules.append(x_rule[y])
 
-    
+    for index in range(cur_index + 1, len(cur_page)):
+        elements_before = cur_page[index]
+        for r in rules:
+            if elements_before == r:
+                return True
+    return False
+
+
+def comes_before(cur_page, cur_index, rule_indexes):
+    cur_num = cur_page[cur_index]
+    rules = []
+    for x in rule_indexes:
+        rules.append(y_rule[x])
+
+    for index in range(0, cur_index):
+        elements_after = cur_page[index]
+        for r in rules:
+            if elements_after == r:
+                return True
+    return False
 
 
 file_data = get_file_data("Day5Input.txt")
@@ -32,9 +55,10 @@ y_rule = []
 middle_pages = 0
 for line in range(file_data.index("") + 1, len(file_data)):
     row = file_data[line].split(",")
+    int_row = []
     for i in range(len(row)):
-        row[i] = int(row[i])
-    pages.append(row)
+        int_row.append(int(row[i]))
+    pages.append(int_row)
 
 for i in range(0, file_data.index("")):
     rule = file_data[i]
@@ -45,24 +69,35 @@ valid_pages = []
 for page in pages:
     valid = True
     i = 0
-    #For each number on the page, find all the rules that relate to that number and check if valid
+    # For each number on the page, find all the rules that relate to that number and check if valid
     while i < len(page):
         num = int(page[i])
-        
-        #check if everything that comes BEFORE this number is valid
-        x_indexes = get_indexes(num, x_rule)
-        
 
-        #check if everything that comes AFTER this number is valid
+        # check if everything that comes AFTER this number is valid
+        #x_indexes contain the indexes of the rules in x_rule that is equal to num
+        x_indexes = get_indexes(num, x_rule)
+        if comes_after(page, i, x_indexes):
+            valid = False
+            print(str(page) + " is invalid (x) at index " + str(i))
+            print()
+            break
+        # check if everything that comes BEFORE this number is valid
+        # y_indexes contain the indexes of the rules in y_rule that is equal to num
         y_indexes = get_indexes(num, y_rule)
-        
-        i+=1
+        if comes_before(page, i, y_indexes):
+            valid = False
+            print(str(page) + " is invalid (y) at index " + str(i))
+            print()
+            break
+
+        i += 1
 
     if valid:
         valid_pages.append(page)
 
-#Takes the middle values of all the valid pages and sums them up
+# Takes the middle values of all the valid pages and sums them up
 for page in valid_pages:
+    print(str(page))
     middle_pages += page[math.floor(len(page) / 2)]
 
 print(middle_pages)
