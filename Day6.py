3e3e3af3e2b4d502bab0afd699c619b035130e34
Harrywjl <146112685+Harrywjl@users.noreply.github.com>
2024-12-17 13:21:16 -0500

    Update Day6.py

--- Day6.py
+++ Day6.py
@@ -1,60 +1,127 @@
-def get_file_data(file_name):
-    f = open(file_name)
-    data = []
-    for line in f:
-        data.append(line.rstrip())
-    return data
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day6 {
+
+    private static String[][] grid;
+    private static int row;
+    private static int col;
+
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/InputFile");
+        int rows = fileData.size();
+        int columns = fileData.get(0).length();
+        grid = new String[rows][columns];
+
+        for (int r = 0; r < grid.length; r++) {
+            for (int c = 0; c < grid[0].length; c++) {
+                grid[r][c] = fileData.get(r).substring(c, c+1);
+            }
+        }
+
+        findGuard();
+        
+    }
 
 
-# for visualization
-def print_map():
-    for i in maze:
-        print(i)
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
 
+    public static void printMap() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
-def move():
-    direction = maze[row][col]
-    r = row
-    c = col
-    i = 1
-    if direction == "^":
-        while maze[r - i][c] == ".":
-            maze[r][c] = "X"
-            maze[r - i][c] = "^"
-            i+=1
-    elif direction == "v":
-        print()
-    elif direction == "<":
-        print()
-    elif direction == ">":
-        print()
+    public static void findGuard() {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j].equals("^") || grid[i][j].equals(">") || grid[i][j].equals("v") || grid[i][j].equals("<")) {
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+        }
+    }
 
-    maze[row][col] = "X"
+    public static void move() {
+        String current = grid[row][col];
+        if (current.equals("^")) {
+            grid[row][col] = "X";
+            if (row - 1 >= 0) {
+                if (grid[row - 1][col].equals(".")) {
+                    grid[row - 1][col] = "^"; 
+                } else {
+                    
+                }
+            }
 
+        } else if (current.equals(">")) {
+            grid[row][col] = "X";
+            if (col + 1 < grid[0].length) {
+                if (grid[row - 1][col].equals(".")) {
+                    grid[row][col + 1] = ">";
+                } else {
+                    if (row + 1 < grid.length || (grid[row + 1][col].equals("."))) {
+                        
+                    }
+                }
+            }
+        } else if (current.equals("v")) {
+            grid[row][col] = "X";
+            if (row + 1 < grid.length) {
+                if (grid[row - 1][col].equals(".")) {
+                    grid[row + 1][col] = "v";
+                } else {
+                    
+                }
+            }
+        } else if (current.equals("<")) {
+            grid[row][col] = "X";
+            if (col - 1 >= 0) {
+                if (grid[row - 1][col].equals(".")) {
+                    grid[row][col - 1] = "<";
+                } else {
+                    
+                }
+            }
+        }
+    }
 
-file_data = get_file_data("Day6Input.txt")
-maze = []
-for line in file_data:
-    r = []
-    r.append("*")
-    for letter in line:
-        r.append(letter)
-    r.append("*")
-    maze.append(r)
-stars = []
-for i in range(len(maze[0])):
-    stars.append("*")
-maze.insert(0, stars)
-maze.append(stars)
+    public static boolean win(int rowIndex, int colIndex) {
+        return (rowIndex < 0 || rowIndex >= grid.length || colIndex < 0 || colIndex >= grid[0].length);
+    }
 
-row = 0
-col = 0
-# gets starting position
-for i in range(len(maze)):
-    for j in range(len(maze[i])):
-        if maze[i][j] == "^":
-            row = i
-            col = j
+    public static int distinctPositions() {
+        int sum = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j].equals("X")) {
+                    sum++;
+                }
+            }
+        }
+        return sum;
+    }
 
-move()
-print_map()
+}
