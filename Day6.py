7e84d15ddf09ca6f565e539e11532c531f48aba9
Harrywjl <146112685+Harrywjl@users.noreply.github.com>
2024-12-18 13:04:14 -0500

    Finished Day6.py Part 1

--- Day6.py
+++ Day6.py
@@ -10,7 +10,7 @@ public class Day6 {
     private static int col;
 
     public static void main(String[] args) {
-        ArrayList<String> fileData = getFileData("src/InputFile");
+        ArrayList<String> fileData = getFileData("src/Day6Input.txt");
         int rows = fileData.size();
         int columns = fileData.get(0).length();
         grid = new String[rows][columns];
@@ -22,7 +22,11 @@ public class Day6 {
         }
 
         findGuard();
-        
+        boolean onMap = true;
+        while (onMap) {
+            onMap = move();
+        }
+        System.out.println(calculateTrail());
     }
 
 
@@ -50,6 +54,7 @@ public class Day6 {
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     public static void findGuard() {
@@ -64,55 +69,55 @@ public class Day6 {
         }
     }
 
-    public static void move() {
+    public static boolean move() {
         String current = grid[row][col];
         if (current.equals("^")) {
             grid[row][col] = "X";
             if (row - 1 >= 0) {
-                if (grid[row - 1][col].equals(".")) {
-                    grid[row - 1][col] = "^"; 
+                if (grid[row - 1][col].equals(".") || grid[row - 1][col].equals("X")) {
+                    grid[row - 1][col] = "^";
+                    row--;
                 } else {
-                    
+                    grid[row][col] = ">";
                 }
             }
-
         } else if (current.equals(">")) {
             grid[row][col] = "X";
             if (col + 1 < grid[0].length) {
-                if (grid[row - 1][col].equals(".")) {
+                if (grid[row][col + 1].equals(".") || grid[row][col + 1].equals("X")) {
                     grid[row][col + 1] = ">";
+                    col++;
                 } else {
-                    if (row + 1 < grid.length || (grid[row + 1][col].equals("."))) {
-                        
-                    }
+                    grid[row][col] = "v";
                 }
             }
         } else if (current.equals("v")) {
             grid[row][col] = "X";
             if (row + 1 < grid.length) {
-                if (grid[row - 1][col].equals(".")) {
+                if (grid[row + 1][col].equals(".") || grid[row + 1][col].equals("X")) {
                     grid[row + 1][col] = "v";
+                    row++;
                 } else {
-                    
+                    grid[row][col] = "<";
                 }
             }
         } else if (current.equals("<")) {
             grid[row][col] = "X";
             if (col - 1 >= 0) {
-                if (grid[row - 1][col].equals(".")) {
+                if (grid[row][col - 1].equals(".") || grid[row][col - 1].equals("X")) {
                     grid[row][col - 1] = "<";
+                    col--;
                 } else {
-                    
+                    grid[row][col] = "^";
                 }
             }
+        } else {
+            return false;
         }
+        return true;
     }
 
-    public static boolean win(int rowIndex, int colIndex) {
-        return (rowIndex < 0 || rowIndex >= grid.length || colIndex < 0 || colIndex >= grid[0].length);
-    }
-
-    public static int distinctPositions() {
+    public static int calculateTrail() {
         int sum = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
